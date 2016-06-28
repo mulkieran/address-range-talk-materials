38973903b114630568686f9ee3aa4cc1351658a9
mulhern <amulhern@redhat.com>
2016-06-28 13:24:53 -0400

    More modifications.
    Signed-off-by: mulhern <amulhern@redhat.com>

--- graph.py
+++ graph.py
@@ -1,38 +1,36 @@
-from collections import defaultdict
+from decimal import Decimal
+from fractions import Fraction
 
-from justbytes import Range
-from justbytes import ValueConfig
-from justbytes import ROUND_DOWN
+from justbases import Radices
 
-def _datum_gen(value, value_config):
-    """
-    Generate an integer datum for given value and value_config.
+import matplotlib.pyplot as plt
 
-    :param int value: a value
-    :param ValueConfig value_config: configuration for representation of value
+def main():
+    ratio = Fraction(1024, 1000)
 
-    :returns: an integer corresponding to the value and value_config
-    :rtype: int
-    """
-    radix = Range(value).getStringInfo(value_config)[0]
-    return radix.as_int(ROUND_DOWN)[0]
+    exponents = range(0, 9)
 
-def data_gen(values, multipliers):
-    """
-    Generate the data for a graph.
+    increases = [ratio ** exp - 1 for exp in exponents]
 
-    :param values: an iterable of values in ascending order
-    :type values: list of int
-    :param multipliers: list of interesting multipliers for units
-    :type multipliers: list of int
+    percentages = [100 * x for x in increases]
 
-    :rtype: dict of str * (dict of str * list of int)
-    :returns: maps from unit multiplier to (IEC, SI) to list of values
-    """
+    radices = [Radices.from_rational(x, 10, 2)[0] for x in percentages] 
 
-    table = defaultdict(dict)
+    rationals = [x.as_rational() for x in radices] 
 
-    for mult in multipliers:
-        for unit_type in (BinaryUnits, DecimalUnits):
-            value_config = ValueConfig(unit_type.unit_for_exp(mult))
-            table[m][unit_type] = [_datum_gen(x, value_config) for x in values]
+    percent_values = \
+       [Decimal(x.numerator)/Decimal(x.denominator) for x in rationals]
+
+    plt.xticks(exponents, ['B', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'])
+    plt.ylabel("Percent Increase: (SI - IEC)/IEC")
+    plt.xlabel("Unit Size")
+    plt.plot(exponents, percent_values) 
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+
+    
